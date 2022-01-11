60237edc96e3485ad1af51fb25b281b862d4cfcf
Deepinder Singh <deepinder@Deepinders-MacBook-Pro.local>
2022-01-11 00:05:57 -0800

    Thinkific Course Picker Branch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.scss";
+import emojiArray from "./emojis.json";
+import twemoji from "twemoji";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [selectedCategory, setSelectedCategory] = useState(0);
+	const [selectedEmoji, setSelectedEmoji] = useState(null);
+
+	function createPickerHeader() {
+		return emojiArray.map((category, index) => (
+			<button
+				className={`emoji-btn ${
+					selectedCategory === index ? "category--active" : ""
+				}`}
+				onClick={() => setSelectedCategory(index)}
+				key={index}
+			>
+				{createSvgIcon(category.unicode, category.name)}
+			</button>
+		));
+	}
+
+	function createPickerBody() {
+		return emojiArray[selectedCategory].emojis.map((emoji, index) => (
+			<button
+				className="emoji-btn"
+				key={index}
+				onClick={() => setSelectedEmoji(emoji)}
+			>
+				{createSvgIcon(emoji.unicode, emoji.name)}
+			</button>
+		));
+	}
+
+	function createSvgIcon(unicode, title) {
+		return (
+			<svg width={25} height={25}>
+				<use href={`/sprite.svg#${unicode}`}>
+					<title>{title}</title>
+				</use>
+			</svg>
+		);
+	}
+
+	function renderEmojiString(char) {
+		const htmlString = twemoji.parse(char, {
+			folder: "svg",
+			ext: ".svg",
+		});
+		return { __html: htmlString };
+	}
+
+	return (
+		<div className="emoji-panel">
+			<header className="emoji-panel__header">{createPickerHeader()}</header>
+			<div className="emoji-panel__body">{createPickerBody()}</div>
+			{selectedEmoji && (
+        <>
+          <div>{JSON.stringify(selectedEmoji)}</div>
+          <div
+            dangerouslySetInnerHTML={renderEmojiString(selectedEmoji.char)}
+          ></div>
+        </>
+			)}
+		</div>
+	);
 }
 
 export default App;
